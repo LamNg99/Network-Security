809727486f93e447ddee0045c8d03a9236226bee
Lam Nguyen <tunglam030699@gmail.com>
2023-03-06 18:44:33 -0500

    lab3

---
+++ Lab3/src/main/java/com/mycompany/lab3/KDCServer.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab3;
+
+/**
+ *
+ * @author lamnguyen
+ */
+
+import java.net.*;
+import java.io.*;
+ 
+public class KDCServer {
+    public static void main(String[] args) throws IOException {
+ 
+    if (args.length != 1) {
+        System.err.println("Usage: java KDCServer <port number>");
+        System.exit(1);
+    }
+ 
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+         
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+            while (listening) {
+                new KDCServerThread(serverSocket.accept()).start();
+            }
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
